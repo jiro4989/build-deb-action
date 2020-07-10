f858f8ca4d81562f047d3ced023ab2cc0292a0c3
jiro4989 <jiroron666@gmail.com>
2020-07-10 09:25:13 +0000

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,24 +3,31 @@
 set -eux
 
 /replacetool \
-  --debian-dir:/template/debian \
+  --debian-dir:/template/DEBIAN \
   --package:"$INPUT_PACKAGE" \
-  --maintainer:"$INPUT_MAINTAINER" \
   --version:"$INPUT_VERSION" \
-  --arch:"$INPUT_ARCH"
+  --arch:"$INPUT_ARCH" \
+  --maintainer:"$INPUT_MAINTAINER" \
+  --description:"$INPUT_DESC"
 
-WORKDIR="/tmp/work"
-PACKAGE_DIR="$WORKDIR/$INPUT_PACKAGE"
+readonly WORKDIR="/tmp/work"
+readonly PACKAGE_DIR="$WORKDIR/$INPUT_PACKAGE"
 
 mkdir -p /tmp/work/
 cp -r /template "$PACKAGE_DIR"
-cp -p "$INPUT_PACKAGE" "$PACKAGE_DIR"
-(
-  cd "$PACKAGE_DIR"
-  /git2chlog deb -o debian/changelog
-  debian/rules build
-  yes | debuild -us -uc
-)
+
+readonly INSTALL_DIR="$PACKAGE_DIR/usr/bin/"
+mkdir -p "$INSTALL_DIR"
+install -m 0755 "$INPUT_PACKAGE" "$INSTALL_DIR"
+
+# remove prefix 'v'
+FIXED_VERSION="$(echo "$INPUT_VERSION" | sed -E 's/^/v/')"
+readonly FIXED_VERSION
+
+# create deb file
+readonly DEB_FILE="${INPUT_PACKAGE}_${FIXED_VERSION}_${INPUT_ARCH}.deb"
+dpkg-deb -b "$PACKAGE_DIR" "$DEB_FILE"
+
 cp -p "$WORKDIR/"*.deb .
 
 ls ./*.deb
