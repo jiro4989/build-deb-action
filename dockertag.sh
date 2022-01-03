1a83d67169278810224b0fa83b01fd5678f1357b
jiro4989 <jiroron666@gmail.com>
2022-01-03 20:28:39 +0900

    Update script

--- dockertag.sh
+++ dockertag.sh
@@ -4,7 +4,6 @@ set -eu
 
 readonly VERSION=$1
 
-readonly BOLD=$'\x1b[1m'
 readonly GREEN=$'\x1b[32m'
 readonly RED=$'\x1b[31m'
 readonly RESET=$'\x1b[m'
@@ -21,15 +20,13 @@ err() {
   echo -e "[$(now)]  ${RED}ERR${RESET}: $*"
 }
 
-if [[ ! "$VERSION" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+if [[ ! "$VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   err "tag version (VERSION = $VERSION) is illegal. tag format must be 'v0.0.0'."
   exit 1
 fi
 
-readonly TAG="docker-$VERSION"
-info "TAG = $TAG"
-git tag "$TAG"
-git push origin "$TAG"
-echo ""
-info "tagging successfully."
-info "see ${BOLD}https://hub.docker.com/r/jiro4989/build-deb-action${RESET} ."
+readonly IMAGE="jiro4989/build-deb-action:$VERSION"
+info "IMAGE = $IMAGE"
+docker build -t "$IMAGE" .
+docker push "$IMAGE"
+info "Successful docker build and push"
