6ff7eec1334c6b97bfe6ba5645a480d17d826459
jiro4989 <jiroron666@gmail.com>
2025-01-14 13:33:41 +0900

    init

---
+++ tools/replacetool/go.mod
@@ -0,0 +1,3 @@
+module github.com/jiro4989/build-deb-action/tools/replacetool
+
+go 1.23.4
---
+++ tools/replacetool/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+type ExitCode int
+
+const (
+	exitCodeOK                  ExitCode = iota
+	exitCodeErrCouldNotOpenFile ExitCode = 10 + iota
+	exitCodeErrInvalidTemplateParam
+	exitCodeErrCouldNotRender
+	exitCodeErrCouldNotCreateFile
+)
+
+func main() {
+	tmplFile := os.Args[1]
+	outFile := os.Args[2]
+	os.Exit(int(Main(tmplFile, outFile)))
+}
+
+func Main(tmplFile string, outFile string) ExitCode {
+	tf, err := os.Open(tmplFile)
+	if err != nil {
+		return exitCodeErrCouldNotOpenFile
+	}
+	defer tf.Close()
+	b, err := io.ReadAll(tf)
+	tmpl := string(b)
+
+	p, err := loadTemplateParam()
+	if err != nil {
+		return exitCodeErrInvalidTemplateParam
+	}
+	s, err := render(tmpl, p)
+	if err != nil {
+		return exitCodeErrCouldNotRender
+	}
+
+	f, err := os.Create(outFile)
+	if err != nil {
+		return exitCodeErrCouldNotCreateFile
+	}
+	defer f.Close()
+	fmt.Fprint(f, s)
+
+	return exitCodeOK
+}
